df947f6d0a5b473f57bd3627949e96a5764a53d0
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-03-19 21:23:12 -0400

    Create flatbokuveisla.rs

---
+++ Kattis/Flatbökuveisla/flatbokuveisla.rs
@@ -0,0 +1,18 @@
+use std::io::stdin;
+
+fn process_input(mut ip: String) -> i32{
+    stdin().read_line(&mut ip).expect("can't find string from stdin");
+
+    let x: i32 = ip.trim().parse().expect("not an int");
+
+    return x;
+
+}
+
+fn main() {
+    let x = process_input(String::new());
+    let y = process_input(String::new());
+
+    println!("{}", x % y);
+
+}
