0c93bbf2b3958f5077d46c8c68ec2b3ac4f88170
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-03-19 21:46:10 -0400

    Create telja.rs

---
+++ Kattis/Telja/telja.rs
@@ -0,0 +1,19 @@
+use std::io::stdin;
+
+fn process_input(mut ip: String) -> i32{
+    stdin().read_line(&mut ip).expect("can't find string from stdin");
+
+    let x: i32 = ip.trim().parse().expect("Can't parse the int");
+
+    return x;
+
+}
+
+fn main() {
+    let x = process_input(String::new());
+
+    for i in 1..x+1{
+        println!("{}", i);
+    }
+
+}
